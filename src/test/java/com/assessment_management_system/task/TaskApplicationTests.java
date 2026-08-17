4c59dc330ace4ccb3adab7d09f9f5b6625eca9f9
RecebRehim <recebrehim05@gmail.com>
2026-08-18 00:59:16 +0300

    chore: bootstrap Spring Boot Maven project

---
+++ src/test/java/com/assessment_management_system/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment_management_system.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
