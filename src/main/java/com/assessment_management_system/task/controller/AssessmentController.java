e29e388157e6e7b72504cec06e28d69a155cacf0
RecebRehim <recebrehim05@gmail.com>
2026-08-19 03:42:53 +0300

    feat: add POST /assessments endpoint

---
+++ src/main/java/com/assessment_management_system/task/controller/AssessmentController.java
@@ -0,0 +1,29 @@
+package com.assessment_management_system.task.controller;
+
+import com.assessment_management_system.task.dto.AssessmentResponse;
+import com.assessment_management_system.task.dto.CreateAssessmentRequest;
+import com.assessment_management_system.task.service.AssessmentService;
+import jakarta.validation.Valid;
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/assessments")
+public class AssessmentController {
+
+    private final AssessmentService assessmentService;
+
+    public AssessmentController(AssessmentService assessmentService) {
+        this.assessmentService = assessmentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AssessmentResponse> create(@Valid @RequestBody CreateAssessmentRequest request) {
+        AssessmentResponse created = assessmentService.create(request);
+        return ResponseEntity.created(URI.create("/assessments/" + created.id())).body(created);
+    }
+}
